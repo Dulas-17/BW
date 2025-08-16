50019ad2b4052c7b5a78e1f100af14c49dbfd472
Dulas <abdullahtshabalala@gmail.com>
2025-08-16 18:10:19 +0200

    Update 12.js

--- 12.js
+++ 12.js
@@ -2,15 +2,12 @@
 const movieData = [
 
   {
-    title: "The Maze Runner ",
-    image: "Images/cover7 .jpeg",
+    title: "The Maze Runner",
+    image: "Images/cover7.jpeg",
     description: "Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment. ",
-    genres: ["Adventure", "Action",
-"Thriller", "Mystery","Sci-Fi"],
-    link: "https://drive.google.com/file/d/1EDZUjrtc-fCUeaLyEx-Bes_wchTs-JAV/preview "
-  },
-
-
+    genres: ["Adventure", "Action", "Thriller", "Mystery", "Sci-Fi"],
+    link: "https://drive.google.com/file/d/1EDZUjrtc-fCUeaLyEx-Bes_wchTs-JAV/preview"
+  }, // <-- Comma added here
 
   {
     title: "Your Name",
@@ -20,7 +17,6 @@ const movieData = [
     link: "https://drive.google.com/file/d/1apFvCPgIKV4t3y27dlzwCUqqMRI8UjJz/preview"
   },
 
-
   {
     title: "The Life List",
     image: "Images/Cover1.jpeg",
@@ -28,7 +24,6 @@ const movieData = [
     genres: ["Drama", "Family", "Adventure"],
     link: "https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview"
   },
-
 
   {
     title: "Power Rangers",
@@ -38,3 +33,27 @@ const movieData = [
     link: "https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview"
   },
 
+  {
+    title: "Fight or Flight",
+    image: "Images/images(1)(22).jpeg",
+    description: "A mercenary takes on the job of tracking a high-value asset known only as \"The Ghost\" on an international flight. Realizing the plane is filled with assassins assigned to kill them both, the pair must work together in a fight for their lives.",
+    genres: ["Action", "Thriller"],
+    link: "https://drive.google.com/file/d/1N0mVKcv5iDy3cEDIvgTb9j9801lQTQc1/preview"
+  },
+
+  {
+    title: "The Swimmers",
+    image: "Images/images(1)(23).jpeg",
+    description: "Two Syrian sisters flee their war-torn home in Damascus, swim for hours in choppy Mediterranean seas to reach Greece as asylum seekers before going on to compete at the Rio Olympic Games.",
+    genres: ["Adventure", "Documentary", "Drama"],
+    link: "https://drive.google.com/file/d/1JkUj0q7_zGiIhegH40aDmYQW8wxmW5mR/preview"
+  },
+
+  {
+    title: "The Kissing Booth",
+    image: "Images/images(1)(24).jpeg",
+    description: "A high school student finds herself face-to-face with her long-term crush when she signs up to run a kissing booth at the spring carnival.",
+    genres: ["Romance", "Comedy"],
+    link: "https://drive.google.com/file/d/1KeGGJbVLWWEXeZH_CAOotK75Wdi-yR7C/preview"
+  },
+];
