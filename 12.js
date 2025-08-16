7560313c84e1b56c9f8a53bcf4c3cd751e4536af
Dulas <abdullahtshabalala@gmail.com>
2025-08-16 17:16:13 +0200

    Update 12.js

--- 12.js
+++ 12.js
@@ -33,4 +33,28 @@ const movieData = [
     genres: ["Action", "Sci-Fi", "Adventure"],
     link: "https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview"
   },
+{
+    title: "Fight or Flight",
+    image: "Images/images(1)(22).jpeg",
+    description: "A mercenary takes on the job of tracking a high-value asset known only as "The Ghost" on an international flight. Realizing the plane is filled with assassins assigned to kill them both, the pair must work together in a fight for their lives . ",
+    genres: [ "Action",
+"Thriller"],
+    link: "https://drive.google.com/file/d/1NOmVKcv5iDy3cEDIvgTb9j9801lQTQc1/preview "
+  },
+
+{
+    title: "The Swimmers",
+    image: "Images/images(1)(23).jpeg",
+    description: "Two Syrian sisters flee their war-torn home in Damascus, swim for hours in choppy Mediterranean seas to reach Greece as asylum seekers before going on to compete at the Rio Olympic Games . ",
+    genres: ["Adventure", "Documentary","Drama"],
+    link: "https://drive.google.com/file/d/1JkUj0q7_zGIiHegH40aDmYQW8wxmW5mR/preview "
+  },
+
+{
+    title: "The kissing booth",
+    image: "Images/images(1)(24).jpeg",
+    description: "A high school student finds herself face-to-face with her long-term crush when she signs up to run a kissing booth at the spring carnival . ",
+    genres: ["Romance", "Comedy"],
+    link: "https://drive.google.com/file/d/1KeGGJbvLWWEXeZH_CAOotK75Wdi-yR7C/preview "
+  },
 ];
